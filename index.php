a627fda07c41d07b5a9989652ae97d6456b45e88
James Ticer <jticer4@icloud.com>
2018-04-13 16:28:10 -0600

    added in my php file and populated it with my persona information

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+echo "<title>Data Design</title>";
+echo "<h1>Data Design</h1>";
+echo "<h3>Persona</h3>
+<h4>Name:</h4>
+<p>Chris P. Bacon</p>
+<h4>Gender:</h4>
+<p>Male</p>
+<h4>Age:</h4>
+<p>30 Years Old</p>
+<h4>Technology:</h4>
+<p>Is using an iPhone 7, running IOS 11 and an HP Envy x360 running the latest 
+version of Windows 10</p>
+<h4>Confidence:</h4>
+<p>Chris is very confident using both his iPhone and his laptop</p>
+<h4>Attitude:</h4>
+<p>Busy, intellectual, who prefers to consume intellectually stimulating content.</p>
+<h4>Goals:</h4>
+<p>To find interesting articles/videos to entertain himself during his lunch break.</p>
+<h4>Frusterations:</h4>
+<p>Chris hates having to scroll through pages of content trying to find something interesting.
+ In his experience a lot of websites post too much filler content.</p>";
+echo "<h3>User Story: </h3>
+<p>This is my test field for my user story. </p>";
+echo "<h3>Use/Case Interaction Flow: </h3>";
+echo "<ul>
+<li>test 1</li>
+<li>test 2</li>
+<li>test 3</li>
+</ul>";
+echo "<h3>Conceptual Model: </h3>
+<p>test field for my conceptual model.</p>";
+?>
