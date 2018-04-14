eb39e862f69cef090136499878901f3c13b48764
James Ticer <jticer4@icloud.com>
2018-04-13 18:28:05 -0600

    added in some periods i was missing

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-echo "<title>Data Design</title>
+echo "
+<title>Data Design</title>
 <h1>Data Design</h1>
 <h3>Persona</h3>
 <h4>Name:</h4>
@@ -9,10 +10,9 @@ echo "<title>Data Design</title>
 <h4>Age:</h4>
 <p>30 Years Old</p>
 <h4>Technology:</h4>
-<p>Is using an iPhone 7, running IOS 11 and an HP Envy x360 running the latest 
-version of Windows 10</p>
+<p>iPhone 7, running IOS 11 and an HP Envy x360 running the latest version of Windows 10.</p>
 <h4>Confidence:</h4>
-<p>Chris is very confident using both his iPhone and his laptop</p>
+<p>Chris is very confident using both his iPhone and his laptop.</p>
 <h4>Attitude:</h4>
 <p>Busy, intellectual, who prefers to consume intellectually stimulating content.</p>
 <h4>Goals:</h4>
@@ -24,23 +24,24 @@ version of Windows 10</p>
 <p>As a user Chris wants to search for content written by a specific author. </p>
 <h3>Use/Case Interaction Flow: </h3>
 <h4>End Goal:</h4>
-<p>Search for content by specific author on Sploid</p>
+<p>Search for content by specific author on Sploid.</p>
 <h4>Description:</h4>
-<p>Chris wants to find content on Sploid posted by Andrew Liszewski</p>
+<p>Chris wants to find content on Sploid posted by Andrew Liszewski.</p>
 <h4>Preconditions: </h4>
-<p>Chris has successfully connected to the internet and opened his web browser</p>
+<p>Chris has successfully connected to the internet and opened his web browser.</p>
 <h4>Postconditions:</h4>
 <p>Chris has a list of content posted by Andrew Liszewski.</p>
 <h4>Interaction Flow:</h4>
 <ul>
 <li>Chris navigates up to the address bar in his browser, types in sploid.gizmodo.com, and 
-hits enter</li>
-<li>The browser returns Sploid's homepage</li>
-<li>Chris navigates to the top left corner of the page and clicks on the magnifying glass icon</li>
-<li>The web browser returns a search field</li>
-<li>Chris clicks on the search field, types in Andrew Liszewski and hits enter</li>
-<li>The web browser returns a list populated with content posted by Andrew Liszewski</li>
+hits enter.</li>
+<li>The browser returns Sploid's homepage.</li>
+<li>Chris navigates to the top left corner of the page and clicks on the magnifying glass icon.</li>
+<li>The web browser returns a search field.</li>
+<li>Chris clicks on the search field, types in Andrew Liszewski and hits enter.</li>
+<li>The web browser returns a list populated with content posted by Andrew Liszewski.</li>
 </ul>
 <h3>Conceptual Model: </h3>
-<p>test field for my conceptual model.</p>";
+<p>test field for my conceptual model.</p>
+";
 ?>
