6c41c8c3d3eb6ba676e3b5d32c518c9bbe6f614b
James Ticer <jticer4@icloud.com>
2018-04-18 10:47:51 -0600

    debugged my sql database

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,18 +1,19 @@
-ALTER DATABASE data_design CHARACTER SET utf8 COLLATE
-utf8_unicode_ci;
+-- CREATE SCHEMA data_design;
+ALTER DATABASE jticer CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 -- drop the tables if they exist
-DROP TABLE IF EXISTS author;
 DROP TABLE IF EXISTS article;
+DROP TABLE IF EXISTS author;
+
 
 -- create the new tables
 CREATE TABLE author (
+	authorId BINARY(16) NOT NULL,
 	authorByline VARCHAR(256),
 	authorEmail VARCHAR(256) NOT NULL,
-	authorId BINARY(16) NOT NULL,
 	authorName VARCHAR(64) NOT NULL,
 	authorTitle VARCHAR(256) NOT NULL,
-	-- create unique indexes so that there is no duplicate data
+	-- create unique index so that there is no duplicate data
 	UNIQUE(authorEmail),
 	-- set my primary key
 	PRIMARY KEY(authorId)
@@ -20,16 +21,15 @@ CREATE TABLE author (
 
 
 CREATE TABLE article (
-	articleAuthorId BINARY(16) NOT NULL ,
-	articleContent VARCHAR(65536) NOT NULL,
-	articleDateTime DATETIME NOT NULL,
-	articleId BINARY(16) NOT NULL ,
+	articleId BINARY(16) NOT NULL,
+	articleAuthorId BINARY(16) NOT NULL,
+	articleContent VARCHAR(1024) NOT NULL,
+	articleDateTime DATETIME(6) NOT NULL,
 	articleTitle VARCHAR(256) NOT NULL,
 	-- index my articleAuthorId before making a foreign key
 	INDEX(articleAuthorId),
-	PRIMARY KEY(articleId)
 	-- create my foreign key
 	FOREIGN KEY(articleAuthorId) REFERENCES author(authorId),
 	-- create my primary key
-
+	PRIMARY KEY(articleId)
 );
