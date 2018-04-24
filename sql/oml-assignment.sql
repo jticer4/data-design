7da0a99147bb0e78a04bea5f6b08e7b9c7a242ab
James Ticer <jticer4@icloud.com>
2018-04-23 19:17:11 -0600

    added my update functions for both the author and the article

--- sql/oml-assignment.sql
+++ sql/oml-assignment.sql
@@ -5,7 +5,7 @@
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError if $pdo is not a PDO connection object
 	 **/
-	public function insert(\PDO $pdo) : void {
+	public function insert($pdo) : void {
 
 		// create query template
 		$query = "INSERT INTO author(authorId, authorByline, authorEmail, authorName, authorTitle) VALUES(:authorId, :authorByline, :authorEmail, :authorName, :authorTitle)";
@@ -22,7 +22,7 @@
 	 * @throws \PDOException when mySQL related errors occur
 	 * @throws \TypeError if $pdo is not a PDO connection object
 	 **/
-	public function insert(\PDO $pdo) : void {
+	public function insert($pdo) : void {
 
 		// create query template
 		$query = "INSERT INTO article(articleId, articleAuthorId, articleContent, articleDateTime, articleTitle) VALUES(:articleId, :articleAuthorId, :articleContent, :articleDateTime, :articleTitle)";
@@ -31,5 +31,38 @@
 		// bind the member variables to the place holders in the template
 		$formattedDate = $this->articleDateTime->format("Y-m-d H:i:s.u");
 		$parameters = ["articleId" => $this->articleId->getBytes(), "articleAuthorId" => $this->articleAuthorId->getBytes(), "articleContent" => $this->articleContent, "articleDateTime" => $formattedDate, "articleTitle" => $this->articleTitle];
+		$statement->execute($parameters);
+	}
+/**
+	 * updates this Author in mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function update($pdo) : void {
+
+		// create query template
+		$query = "UPDATE author SET authorByline = :authorByline, authorEmail = :authorEmail, authorName = :authorName, authorTitle = :authorTitle WHERE authorId = :authorId";
+		$statement = $pdo->prepare($query);
+
+		$parameters = ["authorId" => $this->authorId->getBytes(), "authorByline" => $this->authorByline, "authorEmail" => $this->authorEmail, "authorName" => $this->authorName, "authorTitle" => $this->authorTitle];
+		$statement->execute($parameters);
+	}
+/**
+	 * updates this Article in mySQL
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 **/
+	public function update($pdo) : void {
+
+		// create query template
+		$query = "UPDATE article SET articleAuthorId = :articleAuthorId, articleContent = :articleContent, articleDateTime = :articleDateTime, articleTitle = :articleTitle WHERE articleId = :articleId";
+		$statement = $pdo->prepare($query);
+
+		$formattedDate = $this->articleDateTime->format("Y-m-d H:i:s.u");
+		$parameters = ["articleId" => $this->articleId->getBytes(),"articleAuthorId" => $this->articleAuthorId->getBytes(), "articleContent" => $this->articleContent, "articleDateTime" => $formattedDate, "articleTitle" => $this->articleTitle];
 		$statement->execute($parameters);
 	}
